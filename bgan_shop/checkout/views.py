cb1313a05c14219decfa85238154def0b55f371a
sebastian <sebastian.van.den.noortgaete@gmail.com>
2021-01-27 19:07:34 +0100

    Bancontact payments

--- bgan_shop/checkout/views.py
+++ bgan_shop/checkout/views.py
@@ -3,7 +3,8 @@ from django.contrib.sites.models import Site
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from oscar.apps.checkout.views import PaymentDetailsView as CorePaymentDetailsView
 from oscar.apps.payment.exceptions import RedirectRequired
@@ -11,13 +12,9 @@ from oscar.core.loading import get_model
 from oscar.apps.partner.strategy import Selector
 strategy = Selector().strategy()
 
-from .facade import Facade
-
-from .forms import PaymentType
-
 from . import PAYMENT_METHOD_STRIPE, PAYMENT_EVENT_PURCHASE, STRIPE_EMAIL, STRIPE_TOKEN
 
-import stripe
+import stripe, json
 import uuid
 
 SourceType = get_model('payment', 'SourceType')
@@ -33,7 +30,7 @@ class PaymentDetailsView(CorePaymentDetailsView):
 
     def get_context_data(self, **kwargs):
         current_site = Site.objects.get_current()
-
+        print(self.request.session["payment_method_id"])
         ctx = super().get_context_data(**kwargs)
         if self.preview:
             self.request.session["uuid"] = str(uuid.uuid4())
@@ -64,7 +61,6 @@ class PaymentDetailsView(CorePaymentDetailsView):
                     success_url=f'http://{current_site.domain}/checkout/preview/?completed=1',
                     cancel_url=f'http://{current_site.domain}/checkout/preview/',
                 )
-                print(current_site.domain)
                 self.request.session["session_id"] = session.id
 
         else:
@@ -95,4 +91,59 @@ class PaymentDetailsView(CorePaymentDetailsView):
 
     def payment_metadata(self, order_number, total, **kwargs):
         return {'order_number': order_number}
+
         
+
+# AJAX endpoint when `/pay` is called from client
+@require_http_methods(["POST"])
+@csrf_exempt
+def stripe_pay(request):
+    data = json.loads(request.body)
+    print(data)
+    intent = None
+
+    if data.get("paymentsDetail", False):
+        request.session["payment_method_id"] = data.get("payment_method_id", None)
+        request.session["payment_intent_id"] = data.get("payment_intent_id", None)
+        print("added to the session")
+        return JsonResponse({'next': True}, status=200)
+    else:
+        try:
+            if 'payment_method_id' in request.session:
+                print("payment method id")
+                # Create the PaymentIntent
+                intent = stripe.PaymentIntent.create(
+                    payment_method = request.session['payment_method_id'],
+                    amount = int(request.basket.total_incl_tax * 100),
+                    currency = 'eur',
+                    confirmation_method = 'manual',
+                    confirm = True,
+                )
+                print(intent["id"])
+            elif 'payment_intent_id' in request.session:
+                intent = stripe.PaymentIntent.confirm(request.session['payment_intent_id'])
+        except stripe.error.CardError as e:
+            # Display error on client
+            return JsonResponse({'error': e.user_message}, status=200)
+
+    return generate_response(intent)
+
+def generate_response(intent):
+    # Note that if your API version is before 2019-02-11, 'requires_action'
+    # appears as 'requires_source_action'.
+    if intent.status == 'requires_action' and intent.next_action.type == 'use_stripe_sdk':
+        # Tell the client to handle the action
+        print("requires_action")
+        return JsonResponse({
+            'requires_action': True, 
+            'payment_intent_client_secret': intent.client_secret,
+        }, status=200)
+    elif intent.status == 'succeeded':
+        # The payment didn’t need any additional actions and completed!
+        # Handle post-payment fulfillment
+        print("succeeded")
+        return JsonResponse({'success': True}, status=200)
+    else:
+        # Invalid status
+        print("Invalid PaymentIntent status")
+        return JsonResponse({'error': 'Invalid PaymentIntent status'}, status=500)
--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ from django.views.generic import TemplateView
 from django.apps import apps
 
 from bgan_shop.checkout.hooks import stripe_webhook
+from bgan_shop.checkout.views import stripe_pay
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
@@ -17,6 +18,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # stripe payments webhook
     path("checkout/stripe-webhook/", stripe_webhook, name="stripe_webhook"),
+    path("checkout/stripe-pay/", stripe_pay, name="stripe_pay"),
     path('', include(apps.get_app_config('oscar').urls[0])),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
